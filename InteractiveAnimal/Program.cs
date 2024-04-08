5362ffcbef153fd8a461eac98e31e4bd71633c9c
Maksim <maxpotapnev2003@gmail.com>
2024-04-08 23:04:10 +0300

    AddColor

--- InteractiveAnimal/Program.cs
+++ InteractiveAnimal/Program.cs
@@ -8,17 +8,22 @@ namespace InteractiveAnimal
     {
         public static void Main()
         {
+            Console.ForegroundColor = ConsoleColor.Red;
+
             Console.Write("\tЭта программа реализует производный класс от абстрактного.\n" 
-                + "\tВведите имя собаки.\n\t ");
+                + "\tВведите имя собаки:\n\t ");
+
+            Console.ResetColor();
 
             string? name = Console.ReadLine();
 
             Dog dog = new Dog();
             dog.SetName(name);
 
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"\tИмя собаки: {dog.GetName()}");
-
             dog.Eat();
+            Console.ResetColor();
 
         }
     }
